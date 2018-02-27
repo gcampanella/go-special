502b9dc285b56ecfae7e5b06175dd4966c8fb345
Gianluca Campanella <gianluca@campanella.org>
2018-02-27 14:19:13 +0000

    Reformatted constants.go

--- constants.go
+++ constants.go
@@ -2,10 +2,12 @@ package special
 
 import "math"
 
-const machineEpsilon = 1. / (2 << 52) // 2**-53
+const (
+	machineEpsilon = 1. / (2 << 52) // 2**-53
 
-const maxLog = 1024 * math.Ln2 // log(2**1024)
+	maxLog = 1024 * math.Ln2 // log(2**1024)
 
-const minLog = -1022 * math.Ln2 // log(2**-1022)
+	minLog = -1022 * math.Ln2 // log(2**-1022)
 
-const testDelta = 1. / (2 << 40)
+	testDelta = 1. / (2 << 40)
+)
